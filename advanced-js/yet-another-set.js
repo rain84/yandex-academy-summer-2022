d22e7b6ca7ac29990e5df474ad0192f3a4bac327
rain84 <rainy_sky@mail.ru>
2022-09-22 13:40:11 +0300

    feat: :sparkles: added YetAnotherSet stub

---
+++ advanced-js/yet-another-set.js
@@ -0,0 +1,61 @@
+//  Realization of ES6 Set
+
+const storage = Symbol();
+
+function YetAnotherSet(iterator = []) {
+    if (!new.target) return new YetAnotherSet(iterator);
+
+    this[storage] = [];
+    const s = this[storage];
+    [...iterator].forEach(item => {
+        if (!s.includes(item)) s.push(item);
+    });
+}
+
+module.exports = YetAnotherSet;
+
+YetAnotherSet.prototype.constructor = YetAnotherSet;
+YetAnotherSet.prototype.toString = function () {
+    return '[object ^_^]';
+};
+YetAnotherSet.prototype[Symbol.toStringTag] = '^_^';
+YetAnotherSet.prototype[Symbol.iterator] = function* () {
+    for (const item of this[storage]) yield item;
+};
+YetAnotherSet.prototype.has = function (value) {
+    return this[storage].indexOf(value) !== -1;
+};
+YetAnotherSet.prototype.add = function (value) {
+    this[storage].push(value);
+    return this;
+};
+YetAnotherSet.prototype.delete = function (value) {
+    this[storage] = this[storage].filter((val) => val !== value );
+    return this;
+};
+YetAnotherSet.prototype.clear = function () {
+    this[storage].length = 0;
+    return this;
+};
+YetAnotherSet.prototype.forEach = function (cb, ctx) {
+    this[storage].forEach(cb, ctx);
+    return this;
+};
+YetAnotherSet.prototype.keys = function () {
+    return [...this[storage].keys()]
+};
+YetAnotherSet.prototype.values = function () {
+    return [...this[storage].values()]
+};
+YetAnotherSet.prototype.entries = function () {
+    return [...this[storage].entries()]
+};
+
+Object.defineProperties(YetAnotherSet.prototype, {
+    size: {
+        get: function () {
+            return this[storage].length;
+        },
+        enumerable: false,
+    },
+});
